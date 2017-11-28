c8e4525c3227d8383b6cafbe2ba7194185ed7b81
sparklightSU <zehooves@gmail.com>
2017-11-28 15:50:15 -0800

    Update AreaTester.java
    Critical error found, would break code

--- APCS/src/ch2lab/AreaTester.java
+++ APCS/src/ch2lab/AreaTester.java
@@ -7,6 +7,7 @@ public class AreaTester {
 	public static void main(String[] args){
 		Rectangle r1 = new Rectangle(10,10,50,100);
 		System.out.println("Area: " + (r1.getWidth()*r1.getHeight()));
+		//Haha yes
 		}
 
 }
